3f954357c7cf231fb7e002b617b83cc392e0ee78
orrabadia <orrabadia@gmail.com>
2023-10-19 17:32:54 -0700

    Solved RNA Transcribing Problem with Better Solution and removed white line

--- Transcribing RNA Problem/Transcribing.py
+++ Transcribing RNA Problem/Transcribing.py
@@ -1,15 +1,19 @@
 
-with open ('Transcribing RNA Problem/rosalind_rna.txt') as input:
-    for line in input:
-        for item in line: 
-            if item == 'T':
-                line = line.replace("T", "U")
-        print (line)
+# with open ('Transcribing RNA Problem/rosalind_rna.txt') as input:
+#     for line in input:
+#         for item in line: 
+#             if item == 'T':
+#                 line = line.replace("T", "U")
+#         print (line)
+
+import sys
+file = open('rosalind_rna.txt')
+dna = file.read()
+rna = dna.replace("T", "U")
+print(rna)
     
     
-    # this isn't as fast it could be: this is my initial solution, however I could have used import
-    # sys to open/read the file, then just use .count(nt) for A, G, C, and T.
-    # I also printed inefficiently, I could have just done print(acount,ccount,gcount,tcount)
+
     
             
         
