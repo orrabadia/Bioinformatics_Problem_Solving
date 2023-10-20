f2955a058069521e9b7361d585c42a0ee0730f6b
orrabadia <orrabadia@gmail.com>
2023-10-19 23:10:07 -0700

    Wrong approach previously, updated solution

--- Rabbits and Recurrence Relations/rabbits.py
+++ Rabbits and Recurrence Relations/rabbits.py
@@ -7,17 +7,22 @@
 #  months, if we begin with 1 pair and in each generation, every pair of reproduction-age rabbits produces a litter of k
 #  rabbit pairs (instead of only 1 pair).
 def Rec_Relation(n, k):
-    # handling input validation
-    if (n > 40):
+    if (n > 40): #handle validation
         return "invalid n"
-    if (k > 5):
+    if (k > 5): #handle validation
         return "invalid k"
-    return 1 + k*(n+1) #we start with 1 pair, they make k pairs (1*k) + 
-                    # then each pair in the generation make k pairs for n months (k*n)
+    if (n == 1): #base case
+        return 1
+    elif (n == 2): #second month, 1st pair just matured and
+        return k
+    if (n <= 4): #can be predicted using numbers from 1 and 2 gens previous
+        return Rec_Relation(n-1, k) + Rec_Relation(n-2, k) #Fib
+    return Rec_Relation(n-1, k) + (Rec_Relation(n-2, k) * k) #pattern identification
+    
 
     
     
-file = open("Rabbits and Recurrence Relations/rosalind_fib.txt")
+file = open("rosalind_fib.txt")
 input = file.read()
 inputArr = input.split(" ") #input given as two numbers separated by space
 print(Rec_Relation(int(inputArr[0]), int(inputArr[1])))
